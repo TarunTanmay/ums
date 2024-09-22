834f258ea0206b40b9ffcb2738f442d468eb405c
taruntanmay <taruntanmay730@gmail.com>
2024-09-22 18:31:32 +0530

    auth added to API

--- src/main/java/com/company_name/ums/security/SecurityConfig.java
+++ src/main/java/com/company_name/ums/security/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.company_name.ums.security;
-import com.company_name.ums.exception.ErrorResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,19 +20,9 @@ public class SecurityConfig{
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/v1/user/**").hasRole("USER")
                 .antMatchers("/api/v1/login", "/api/v1/register").permitAll()
                 .anyRequest().authenticated() // Protect all other endpoints
                 .and()
-//                .exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
-//                    System.out.println(request.getHeader("token"));
-//                    ObjectMapper mapper = new ObjectMapper();
-//                    ErrorResponse errorResponse = new ErrorResponse(response.getStatus(),authException.getMessage());
-//                    response.setHeader("content-type", "application/json");
-//                    response.setStatus(response.getStatus());
-//                    String responseMsg = mapper.writeValueAsString(errorResponse);
-//                    response.getWriter().write(responseMsg);})
-//                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilterBefore(userTokenAuthFilter, BasicAuthenticationFilter.class)
--- src/main/java/com/company_name/ums/security/UserTokenAuthFilter.java
+++ src/main/java/com/company_name/ums/security/UserTokenAuthFilter.java
@@ -2,19 +2,16 @@ package com.company_name.ums.security;
 
 import com.company_name.ums.exception.BadEntryException;
 import com.company_name.ums.exception.ErrorResponse;
-import com.company_name.ums.exception.UnauthorizedException;
 import com.company_name.ums.model.User;
 import com.company_name.ums.model.UserLogin;
 import com.company_name.ums.repository.UserLoginRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -34,67 +31,51 @@ public class UserTokenAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getRequestURI();
-        if (authHeader != null && (authHeader.startsWith("/api/v1/login") || authHeader.startsWith("/api/v1/register"))) {
+        System.out.println("doFilterInternal");
+        String path = request.getRequestURI();
+        if (path.startsWith("/api/v1/login") || path.startsWith("/api/v1/signup")) {
             filterChain.doFilter(request, response);
-            return;
+           return;
         }
 
-
-        String accessToken = request.getHeader("token");
-        if (validateToken(!userLoginRepository.findByToken(accessToken).isPresent(), HttpStatus.BAD_REQUEST, "Invalid token", response))
-            return;
-
-        UserLogin token = userLoginRepository.findByToken(accessToken).orElseThrow(() ->
-                new BadEntryException("User not found")
+        String token = extractTokenFromRequest(request);
+        Collection<GrantedAuthority> authorities = Arrays.asList(
+                new SimpleGrantedAuthority("ROLE_USER"),
+                new SimpleGrantedAuthority("ROLE_ADMIN")
         );
-        if (validateToken(token.isExpired(), HttpStatus.UNAUTHORIZED, "Token expired", response)) return;
 
-        User user = token.getUser();
-        if (user != null) {
-            try {
-                if (null == SecurityContextHolder.getContext().getAuthentication()) {
-                    if (!user.isDeleted()) {
-                        Collection<GrantedAuthority> authorities = Arrays.asList(
-                                new SimpleGrantedAuthority("ROLE_USER"),
-                                new SimpleGrantedAuthority("ROLE_ADMIN")
-                        );
-                        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                                new UsernamePasswordAuthenticationToken(
-                                        user.getId(), null, authorities);
-                        usernamePasswordAuthenticationToken
-                                .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                        SecurityContextHolder.getContext()
-                                .setAuthentication(usernamePasswordAuthenticationToken);
-                    }
-                }
-            } catch (IllegalArgumentException e) {
-                logger.error("Unable to fetch JWT Token");
-                throw new BadEntryException("Unable to fetch JWT Token");
-            } catch (ExpiredJwtException e) {
-                logger.error("JWT Token is expired");
-                throw new BadEntryException("JWT Token is expired");
-            } catch (Exception e) {
-                logger.error(e.getMessage());
-                throw new BadEntryException(e.getMessage());
+        try {
+            UserLogin userLogin = userLoginRepository.findByToken(token).orElseThrow(() -> new BadEntryException("Invalid token"));
+            if (!userLogin.isExpired() && !userLogin.isDeleted()) {
+                User user = userLogin.getUser();
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(user.getId(), null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } else {
+                ObjectMapper mapper = new ObjectMapper();
+                ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), "unauthorized");
+                response.setHeader("content-type", "application/json");
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                String responseMsg = mapper.writeValueAsString(errorResponse);
+                response.getWriter().write(responseMsg);
+                return;
             }
-        } else {
-            logger.warn("JWT Token missing");
-            throw new BadEntryException("user Token missing");
+
+        } catch (Exception e) {
+            ObjectMapper mapper = new ObjectMapper();
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Invalid Token Status");
+            response.setHeader("content-type", "application/json");
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            String responseMsg = mapper.writeValueAsString(errorResponse);
+            response.getWriter().write(responseMsg);
+            return;
         }
+
         filterChain.doFilter(request, response);
     }
 
-    private boolean validateToken(boolean userLoginRepository, HttpStatus badRequest, String Invalid_token, HttpServletResponse response) throws IOException {
-        if (userLoginRepository) {
-            ObjectMapper mapper = new ObjectMapper();
-            ErrorResponse errorResponse = new ErrorResponse(badRequest.value(), Invalid_token);
-            response.setHeader("content-type", "application/json");
-            response.setStatus(badRequest.value());
-            String responseMsg = mapper.writeValueAsString(errorResponse);
-            response.getWriter().write(responseMsg);
-            return true;
-        }
-        return false;
+    private String extractTokenFromRequest(HttpServletRequest request) {
+        return request.getHeader("token");
     }
 }
